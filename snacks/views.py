367fb733205574c401b573a0e69f5f177ca2780a
sajedaalrababah <sajedaalrababah@icloud.com>
2023-07-12 00:22:50 +0300

    s1

---
+++ snacks/views.py
@@ -0,0 +1,15 @@
+from django.views.generic import ListView, DetailView
+from .models import Snack
+
+# Create your views here.
+
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+    context_object_name = 'snacks_list'
+
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
